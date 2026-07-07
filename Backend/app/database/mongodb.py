5dacdcf1d4cb277ced568740ec5b14fbf900bc77
sivagnanamJanusika <janusikajanu82@gmail.com>
2026-07-07 10:17:28 +0530

    mongodb file create

---
+++ Backend/app/database/mongodb.py
@@ -0,0 +1,8 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from ..config import settings
+
+client = AsyncIOMotorClient(settings.MONGODB_URI)
+db = client[settings.MONGODB_DB]
+
+users_collection = db["users"]
+tasks_collection = db["tasks"]
