0c472336cb60dec49cbbdbff735850e172a4f633
Asmitha <asmithathavaradnam@gmail.com>
2026-07-07 15:20:21 +0530

    change folder structure

--- Backend/app/utils/role_checker.py
+++
@@ -1,5 +0,0 @@
-from fastapi import HTTPException, status
-
-def require_role(user: dict, role: str):
-    if user.get("role") != role:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Insufficient permissions")
